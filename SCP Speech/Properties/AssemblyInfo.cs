b78bfa18bec270a23ae19d33fdebdac58ccd186b
Artem Košilinsky <46756889+ITeMbI4@users.noreply.github.com>
2020-09-06 07:49:23 +0200

    1.2 Update

--- SCP Speech/Properties/AssemblyInfo.cs
+++ SCP Speech/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SCPSpeech")]
+[assembly: AssemblyTitle("SCP Speech")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SCPSpeech")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
