479e4e6762e2e261e1ca90388bf6a1cfe5e40343
Artem Košilinsky <46756889+ITeMbI4@users.noreply.github.com>
2020-09-06 07:13:51 +0200

    1.2 Update

--- SCP Speech/SCPSpeech.cs
+++ SCP Speech/SCPSpeech.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using Smod2;
 using Smod2.Attributes;
 
@@ -9,7 +9,7 @@ namespace SCP_Speech
         name = "SCP Speech",
         description = "Allow certain SCP to speak with other classes.",
         id = "TeMbI4.SCP.Speech",
-        version = "1.1",
+        version = "1.2",
         SmodMajor = 3,
         SmodMinor = 8,
         SmodRevision = 4
@@ -18,6 +18,14 @@ namespace SCP_Speech
     {
         public static SCPSpeech plugin;
         public Harmony Instance;
+
+        public static bool Enabled;
+        public static bool s939I;
+        public static bool s049;
+        public static bool s0492;
+        public static bool s096;
+        public static bool s106;
+        public static bool s173;
 
         public override void OnEnable() 
         {
@@ -31,23 +39,22 @@ namespace SCP_Speech
             Instance.UnpatchAll(); 
             this.Info("SCP Speech has been unloaded"); 
         }
-        public static bool s049;
-        public static bool enabled;
-        public static bool s0492;
-        public static bool s096;
-        public static bool s106;
-        public static bool s173;
+
         public override void Register()
         {
             this.AddEventHandlers(new EventHandlers(this));
-            this.AddConfig(new Smod2.Config.ConfigSetting("sp_enabled", true, true, "no description"));
-            this.AddConfig(new Smod2.Config.ConfigSetting("sp_049", true, true, "no description"));
-            this.AddConfig(new Smod2.Config.ConfigSetting("sp_0492", true, true, "no description"));
-            this.AddConfig(new Smod2.Config.ConfigSetting("sp_096", true, true, "no description"));
-            this.AddConfig(new Smod2.Config.ConfigSetting("sp_106", true, true, "no description"));
-            this.AddConfig(new Smod2.Config.ConfigSetting("sp_173", true, true, "no description"));
+
+            this.AddConfig(new Smod2.Config.ConfigSetting("sp_enabled", true, true, "Is plugin enabled?"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("sp_939intercom", true, true, "Can SCP-939 use Intercom?"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("sp_049", true, true, "Can SCP-049 speak?"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("sp_0492", false, true, "Can SCP-049-2 speak?"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("sp_096", false, true, "Can SCP-096 speak?"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("sp_106", false, true, "Can SCP-106 speak?"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("sp_173", false, true, "Can SCP-173 speak?"));
+
+            Enabled = this.GetConfigBool("sp_enabled");
+            s939I = this.GetConfigBool("sp_939intercom");
             s049 = this.GetConfigBool("sp_049");
-            enabled = this.GetConfigBool("sp_enabled");
             s0492 = this.GetConfigBool("sp_0492");
             s096 = this.GetConfigBool("sp_096");
             s173 = this.GetConfigBool("sp_173");
